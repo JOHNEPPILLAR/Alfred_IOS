44101e611a439d0cfcceff183555e104c358c27f
John Pillar <johneppillar@gmail.com>
2018-07-10 20:35:39 +0100

    Updated commute screen

--- Alfred_IOS/TableCells/CommuteTableViewCell.swift
+++ Alfred_IOS/TableCells/CommuteTableViewCell.swift
@@ -12,41 +12,42 @@ import SVProgressHUD
 class CommuteTableViewCell: UITableViewCell {
     
     @IBOutlet weak var lineType: UIImageView!
-    @IBOutlet weak var Summary: UILabel!
-    @IBOutlet weak var DepartTime: UILabel!
-    @IBOutlet weak var Duration: UILabel!
+    @IBOutlet weak var Summary1: UILabel!
+    @IBOutlet weak var Summary2: UILabel!
     @IBOutlet weak var Distruptions: UIImageView!
     
     var DistruptionText:String!
     
     func configureWithItem(item: CommuteLegs) {
         if item.status == "No trains running" {
-            DepartTime.isHidden = true
-            Duration.isHidden = true
-            Summary.frame.origin = CGPoint(x: 54, y: 25)
-            Summary.text = item.status
             lineType.image = #imageLiteral(resourceName: "ic_travel_train")
+            Summary1.isHidden = true
+            Summary2.frame.origin = CGPoint(x: 54, y: 25)
+            Summary2.text = item.status
         } else {
-            DepartTime.isHidden = false
-            Duration.isHidden = false
-            Summary.frame.origin = CGPoint(x: 54, y: 35)
+            Summary1.isHidden = false
+            Summary2.frame.origin = CGPoint(x: 54, y: 35)
+            Summary2.isHidden = false
             Distruptions.image = nil
             switch(item.mode!) {
                 case "train" :
                     lineType.image = #imageLiteral(resourceName: "ic_travel_train")
                     if item.line == "Thameslink" { lineType.image = #imageLiteral(resourceName: "ic_travel_crossrail") }
-                    Summary.text = item.departureStation! + " to " + item.arrivalStation!
+                    Summary1.text = item.departureTime! + "-" + item.arrivalTime! + " (" + "\(item.duration!)" + " min) (" + item.status! + ")"
+                    Summary2.text = item.departureStation! + " (pl " + item.departurePlatform! + ") to " + item.arrivalStation!
                     break;
                 case "tube" :
                     lineType.image = #imageLiteral(resourceName: "ic_travel_underground")
-                    Summary.text = item.line! + " from " + item.departureStation! + " to " + item.arrivalStation!
+                    Summary1.text = item.departureTime! + "-" + item.arrivalTime! + " " + " (" +  "\(item.duration!)" + " min)"
+                    Summary2.text = item.line! + " from " + item.departureStation! + " to " + item.arrivalStation!
                     break;
                 case "bus" :
                     lineType.image = #imageLiteral(resourceName: "ic_travel_bus")
                     break;
                 case "walk" :
+                    Summary1.text = item.departureTime! + "-" + item.arrivalTime! + " (" +  "\(item.duration!)" + " min)"
+                    Summary2.text = item.departureStation! + " to " + item.arrivalStation!
                     lineType.image = #imageLiteral(resourceName: "ic_walk")
-                    Summary.text = item.departureStation! + " to " + item.arrivalStation!
                     break;
                 case "dlr" :
                     lineType.image = #imageLiteral(resourceName: "ic_travel_dlr")
@@ -63,9 +64,6 @@ class CommuteTableViewCell: UITableViewCell {
                 default:
                     lineType.image = #imageLiteral(resourceName: "ic_question_mark")
             }
-            
-            DepartTime.text = item.departureTime! + " - " + item.arrivalTime!
-            Duration.text = "(" +  "\(item.duration!)" + " min)"
         }
         if item.disruptions == "true" { Distruptions.image = #imageLiteral(resourceName: "ic_info") }
     }
